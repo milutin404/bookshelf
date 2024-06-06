0f974da2146ce83336b3ec12b87372241fad70e6
Milutin Petrovic <milutin404@gmail.com>
2024-06-06 15:39:05 +0200

    added fetch

--- src/components/search/Search.js
+++ src/components/search/Search.js
@@ -1,9 +1,17 @@
 function Search() {
 
-    function doTheSearch(event) {
-        
-        console.log(event.target.value);
+    let searchOnTimeout;
+    const timerForSearch = 700;
 
+    function doTheSearch(event) {
+        clearTimeout(searchOnTimeout);
+        searchOnTimeout = setTimeout(() => {
+            fetch('http://openlibrary.org/search.json?q=' + event.target.value)
+                .then(value => value.json())
+                .then(response => {
+                    console.table(response);
+                });
+        }, timerForSearch);
     }
 
     return (
